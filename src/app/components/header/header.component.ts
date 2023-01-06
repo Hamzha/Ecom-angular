d23554d983d182305a688489c2f8d55718075181
Hamza Ahmed <ahmedahmza884@gmail.com>
2023-01-06 12:20:15 +0500

    cart

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import { Cart, CartItem } from "src/app/models/cart.model";
+import { CartService } from "src/app/services/cart.service";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
+  selector: "app-header",
+  templateUrl: "./header.component.html",
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  private _cart: Cart = {
+    items: [],
+  };
+  itemsQuantity: number = 0;
 
+  @Input()
+  get cart(): Cart {
+    return this._cart;
+  }
+  set cart(value: Cart) {
+    this._cart = value;
+    this.itemsQuantity = value.items
+      .map((item) => item.quantity)
+      .reduce((prev, current) => prev + current, 0);
+  }
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    throw new Error("Method not implemented.");
+  }
+
+  getTotal(items: Array<CartItem>): number {
+    return this.cartService.getTotal(items);
+  }
+
+  onClearCart(): void{
+    this.cartService.clearCart();
+  }
 }
--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Cart, CartItem } from "src/app/models/cart.model";
+import { CartService } from "src/app/services/cart.service";
 
 @Component({
   selector: "app-cart",
@@ -41,13 +42,32 @@ export class CartComponent implements OnInit {
     "total",
     "action",
   ];
+
+  constructor(private cartService: CartService) {}
+
   ngOnInit() {
-    this.dataSource = this.cart.items;
+    this.cartService.cart.subscribe((_cart) => {
+      this.cart = _cart;
+      this.dataSource = this.cart.items;
+    });
   }
 
   getTotal(items: Array<CartItem>): number {
-    return items
-      .map((item) => item.price * item.quantity)
-      .reduce((prev, current) => prev + current, 0);
+    return this.cartService.getTotal(items);
+  }
+  clearCart(): void {
+    this.cartService.clearCart();
+  }
+
+  removeCart(item: CartItem): void {
+    this.cartService.removeQuantity(item);
+  }
+
+  addQuantity(item: CartItem): void {
+    this.cartService.addQuantity(item);
+  }
+
+  removeQuantity(item: CartItem): void {
+    this.cartService.removeQuantity(item);
   }
 }
